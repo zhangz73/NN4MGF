71af1622d12b59fcb3bb5f1e2d4ee1e4a759707a
Zhanhao Zhang <zz564@alpha1.cm.cluster>
2026-01-17 22:02:29 -0500

    add results

--- test_skew_symmetric.py
+++ test_skew_symmetric.py
@@ -256,8 +256,8 @@ if RETRAIN:
         dict(epochs=800, lr=2e-4, T0=800, eta_min=2e-5),
         dict(epochs=800, lr=7e-5, T0=800, eta_min=7e-6),
     ]
-    lam_monotone = 1e1
-    lam_CR = 1e1
+    lam_monotone = 1e0
+    lam_CR = 1e0
     lam_growth = 0
     lam_zero_anchor = 1e-1
 #    joint_rounds = [

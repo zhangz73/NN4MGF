de77f4f7ed1473f3c2f9226f16b90e7d145a3e7f
Zhanhao Zhang <zz564@alpha1.cm.cluster>
2026-01-27 22:18:28 -0500

    add results

--- test_skew_symmetric.py
+++ test_skew_symmetric.py
@@ -289,7 +289,7 @@ if RETRAIN:
         # Warm-up / coarse fit
         dict(epochs=40000, lr=1e-3, T0=40000, eta_min=1e-5),
         # Refine BAR fit
-        #dict(epochs=20000, lr=1e-4, T0=20000, eta_min=3e-5),
+        dict(epochs=40000, lr=1e-5, T0=40000, eta_min=1e-7),
 #        # Final polishing
         #dict(epochs=20000, lr=3e-5, T0=20000, eta_min=1e-5),
         #dict(epochs=20000, lr=1e-5, T0=20000, eta_min=3e-6),

a83a08dbdb4b861f696791a28c2746d179042fa4
Zhanhao Zhang <zz564@alpha1.cm.cluster>
2026-02-03 19:41:09 -0500

    add intermediate results

--- test_skew_symmetric.py
+++ test_skew_symmetric.py
@@ -363,7 +363,7 @@ if RETRAIN:
     lam_growth = 0
     lam_zero_anchor = 1e-1
     lam_boundary_consistent = 1e1
-    batch_size = 25000 #8192#16384
+    batch_size = 16384 #8192#16384
     train_freq = 2
 #    joint_rounds = [
 #        dict(epochs=5000, lr=1e-3, T0=5000, eta_min=1e-5),

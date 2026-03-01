1e9f7b15d01b33b422adef4f0c77b8106392bd4d
Zhanhao Zhang <zhangz73@cs.washington.edu>
2026-03-01 13:38:58 -0500

    add latest results from empire ai

--- fit_mgf.py
+++ fit_mgf.py
@@ -799,8 +799,8 @@ class MGFTrainer:
             # oversample to reduce rejection loops
             n_try = int(remaining) #int(remaining * 1.5) + 16
 
-            kappa = self.d / 20
-            real_lb_curr = (ub - lb) * torch.rand(n_try, 1, device=self.device).double() ** (1 / (kappa)) + lb
+            kappa = self.d ** 0.5
+            real_lb_curr = ub - (ub - lb) * torch.rand(n_try, 1, device=self.device).double() ** (kappa)
             real_ub_curr = ub
             imag_bd = max(abs(imag_ub), abs(imag_lb)) * torch.rand(n_try, 1, device=self.device).double()
             #real = (real_ub_curr - real_lb_curr) * self.antithetic_uniform(n_try, self.d) + real_lb_curr

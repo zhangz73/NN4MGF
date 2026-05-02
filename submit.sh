13ba94fbf4fdf2e1eef71d8127e1124d95a4d38c
Zhanhao Zhang <zhangz73@cs.washington.edu>
2026-05-02 09:31:48 -0400

    add intermediate results

--- submit.sh
+++ submit.sh
@@ -5,10 +5,10 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-node=1  # Request 1 GPU (max 8)
-#SBATCH --time=18:00:00     # Time limit hrs:min:sec
+#SBATCH --time=24:00:00     # Time limit hrs:min:sec
 #SBATCH --output=job_%j.out
 #unset CUDA_MPS_PIPE_DIRECTORY CUDA_MPS_LOG_DIRECTORY
 
 # Run your command
-srun -u python3 test_skew_symmetric.py 20
+srun -u python3 test_skew_symmetric.py 30
 
